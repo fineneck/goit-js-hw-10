77a98d189ed09382d29666fcfa36b9eae601290e
OleksiiMikhieiev <babybong0993@gmail.com>
2023-03-24 03:45:04 +0200

    add_js-file+add_libraries

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import cardCounties from './templates/cardsCountries.hbs';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+
+console.log(cardCounties);
+
