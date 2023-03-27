13f955cbc2deb4795a213704213d964601446e9d
OleksiiMikhieiev <babybong0993@gmail.com>
2023-03-27 18:34:21 +0300

    check

--- src/index.js
+++ src/index.js
@@ -16,50 +16,13 @@ refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 function onInput(e) {
   e.preventDefault();
 
-  inputValue = e.target.value.trim();
   const searchQuery = e.target.value.trim();
 
-  if (inputValue !== '') {
-    fetchCountries(inputValue);
-  } else {
   if (searchQuery === '') {
     clearField.clearAllFields();
     return;
   }
 
-export function renderCountryCard(countries) {
-  if (countries.length >= 10) {
-    Notify.info('Too many matches found. Please enter a more specific name.');
-  };
-  clearField.clearAllFields();
-
-  if (countries.length === 1) {
-    const countryMarkup = countries.flatMap(({ flags, languages, population, capital, name }) => {
-      return `
-      <h1>
-        <img
-          src='${flags.svg}'
-          alt='${flags.svg}'
-          width='40'
-          heigth='25'
-        />
-        ${name.official}
-      </h1>
-      <ul>
-        <li>
-        <p>Capital:</p>${capital}
-        </li>
-        <li>
-        <p>Population:</p>${population}
-        </li>
-        <li>
-        <p>Languages:</p>${languages}
-        </li>
-      </ul>`;
-    }).join('');
-    refs.countryInfo.innerHTML = countryMarkup;
-    clearField.clearcountryInfo();
-  }
   fetchCountries(searchQuery).then(country => {
     if (country.length >= 11) {
       Notify.info('Too many matches found. Please enter a more specific name.');
